9e62bcb7c0b062c815683ed5fc6d35bb8ad9d707
jrafaaael <jrmd070102@gmail.com>
2023-11-13 12:54:45 -0400

    fix: register maintenance instead of properties

--- register.php
+++ register.php
@@ -5,26 +5,43 @@ $db = new DB();
 
 // Procesar el formulario de registro
 if (isset($_POST['register'])) {
-    $code = $_POST['code'];
-    $state = $_POST['state'];
+    $code = $_POST['property_id'];
+    $type = $_POST['type'];
+    $technician = $_POST['technician'];
+    $date = $_POST['date'];
+    $nextMaintenance = $_POST['next-maintenance'];
+    $reason = $_POST['reason'];
     $description = $_POST['description'];
+    $notes = $_POST['notes'];
 
     // Validación de los datos del formulario
     $errors = [];
     if (empty($code)) {
         die("Error: El nombre es obligatorio. ir atrás para corregir.");
     }
-    if (empty($state)) {
+    if (empty($type) || !array_key_exists($type, $db->getListMaintenanceType())) {
+        die("Error: El apellido es obligatorio. ir atrás para corregir.");
+    }
+    if (empty($technician) || !array_key_exists($technician, $db->getListMaintenanceTechnician())) {
+        die("Error: El apellido es obligatorio. ir atrás para corregir.");
+    }
+    if (empty($date)) {
+        die("Error: El apellido es obligatorio. ir atrás para corregir.");
+    }
+    if (empty($reason)) {
         die("Error: El apellido es obligatorio. ir atrás para corregir.");
     }
     if (empty($description)) {
         die("Error: El correo electrónico es obligatorio. ir atrás para corregir.");
     }
+    if (empty($notes)) {
+        die("Error: El correo electrónico es obligatorio. ir atrás para corregir.");
+    }
 
     try {
         $db->create(
-            "INSERT INTO goods (code, state, description) VALUES (?, ?, ?)",
-            [$code, $state, $description]
+            "INSERT INTO maintenances (property_id, type, description, date, technician, next_maintenance_date, failure_reason, notes) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+            [$code, $type, $description, $date, $technician, $nextMaintenance, $reason, $notes]
         );
 
         header("Location: dashboard.php");
--- registerh.php
+++ registerh.php
@@ -1,3 +1,9 @@
+<?php
+
+include('conn.php');
+$db = new DB();
+?>
+
 <html lang="en" class="h-100" data-bs-theme="light">
 
 <head>
@@ -22,28 +28,71 @@
                         <form action="register.php" method="POST">
 
                             <div class="container-fluid">
+                                <div class="row">
+                                    <div class="form-group pb-2">
+                                        <label for="property_id">Código:</label>
+                                        <input type="text" name="property_id" id="property_id" class="form-control" required>
+                                    </div>
+                                </div>
 
                                 <div class="row">
                                     <div class="form-group pb-2">
-                                        <label for="code">Código:</label>
-                                        <input type="text" name="code" id="code" class="form-control" required>
+                                        <label for="type">Tipo de mantenimiento:</label>
+                                        <select class="form-control" name="type" id="type">
+                                            <?php foreach ($db->getListMaintenanceType() as $key => $value) : ?>
+                                                <option value=<?php echo $key; ?>><?php echo $value; ?></option>
+                                            <?php endforeach; ?>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="form-group pb-2">
+                                        <label for="technician">Técnico:</label>
+                                        <select class="form-control" name="technician" id="technician">
+                                            <?php foreach ($db->getListMaintenanceTechnician() as $key => $value) : ?>
+                                                <option value=<?php echo $key; ?>><?php echo $value; ?></option>
+                                            <?php endforeach; ?>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="form-group pb-2">
+                                        <label for="date">Fecha de inicio:</label>
+                                        <input required type="datetime-local" name="date" id="date" class="form-control" value=<?php echo date('Y-m-d\TH:i') ?>>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="form-group pb-2">
+                                        <label for="reason">Razón del mantenimiento:</label>
+                                        <input type="text" name="reason" id="reason" class="form-control" required>
                                     </div>
                                 </div>
 
                                 <div class="row">
                                     <div class="form-group pb-2">
                                         <label for="description">Descripción:</label>
-                                        <textarea name="description" id="description" rows="5" class="form-control" required></textarea>
+                                        <textarea name="description" id="description" rows="5" class="form-control no-resize" required></textarea>
                                     </div>
                                 </div>
 
                                 <div class="row">
                                     <div class="form-group pb-2">
-                                        <label for="state">Estado:</label>
-                                        <select class="form-control" name="state" id="state">
-                                            <option value="bueno">Bueno</option>
-                                            <option value="malo">Malo</option>
-                                        </select>
+                                        <label for="notes">Notas:</label>
+                                        <textarea name="notes" id="notes" rows="5" class="form-control no-resize" required></textarea>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="form-group pb-2">
+                                        <label for="next-maintenance">
+                                            <abbr title="Valor sugerido para el primer lunes dentro de 3 meses">
+                                                Siguiente mantenimiento:
+                                            </abbr>
+                                        </label>
+                                        <input type="date" name="next-maintenance" id="next-maintenance" class="form-control">
                                     </div>
                                 </div>
 
